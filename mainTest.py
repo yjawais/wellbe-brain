bc2cab92df0524e1c98e0c26838821f6698250c1
yjawais <yjawais@gmail.com>
2022-11-10 23:45:31 +0530

    initial commit

---
+++ mainTest.py
@@ -0,0 +1,25 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+
+model=load_model('BrainTumor10Epochscategorical.h5')
+
+image=cv2.imread('pred\pred3.jpg')
+
+img=Image.fromarray(image)
+
+img=img.resize((64,64))
+
+img=np.array(img)
+
+input_img=np.expand_dims(img,axis=0)
+
+#print(img)
+
+result = model.predict(input_img)
+result_final=np.argmax(result,axis=1)
+print(result)
+
+
